77cb36804100b6177f6355a57c40c1761d957a78
TNKharu <2230294@ecc.ac.jp>
2025-06-13 10:36:42 +0900

    [update]clrclesのmodel修正

--- containers/gcore/src/models/circles.go
+++ containers/gcore/src/models/circles.go
@@ -4,15 +4,15 @@ import "time"
 
 // テーブル定義
 type Circle struct {
-	CircleID string    `gorm:"varchar(36);primaryKey" json:"circlesID"`    // ゲームID
-	GameID    string    `gorm:"varchar(36) not null" json:"gameID"` // ゲーム開始時間
-	UserID    string    `gorm:"varchar(36) not null" json:"userID"` // ゲーム終了時間
-	Size      int       `gorm:"int not null" json:"size"`           // ゲームユニット 0:個人戦、1:チーム戦
-	Level     int       `gorm:"int not null" json:"level"`          // ゲームタイプ	0:system、1:admin
-	Latitude  float64   `gorm:"double" json:"latitude"`             // 参加チーム
-	Longitude float64   `gorm:"double" json:"longitude"`            // ゲームステータス　0:開始前、1:開催中、2:終了済
-	CreatedAT time.Time `gorm:"autoCreateTime" json:"createdAT"`            // ゲーム開催地域
-	ImageID   string    `gorm:"varchar(36)" json:"imageID"`         // ゲーム開催地域
+	CircleID  string    `gorm:"varchar(36);primaryKey" json:"circlesID"` // サークルID
+	GameID    string    `gorm:"varchar(36) not null" json:"gameID"`      // ゲームID
+	UserID    string    `gorm:"varchar(36) not null" json:"userID"`      // ユーザーID
+	Size      int       `gorm:"not null" json:"size"`                    // サークルサイズ
+	Level     int       `gorm:"not null" json:"level"`                   // 防衛レベル
+	Latitude  float64   `gorm:"double" json:"latitude"`                  // 緯度
+	Longitude float64   `gorm:"double" json:"longitude"`                 // 経度
+	CreatedAT time.Time `gorm:"autoCreateTime" json:"createdAT"`         // 作成時
+	ImageID   string    `gorm:"varchar(36)" json:"imageID"`              // イメージID
 }
 
 // テーブル名
