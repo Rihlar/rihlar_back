60eb3fc9b141a3dfd89e347c787199795902759b
mattuu0 <tempacountyt@gmail.com>
2025-06-30 10:46:08 +0900

    円のレベルを変えた時に関連するチャンクのレベルを更新するように変更

--- containers/gcore/src/models/circles.go
+++ containers/gcore/src/models/circles.go
@@ -16,11 +16,20 @@ type Circle struct {
 	Longitude float64   `gorm:"double" json:"longitude"`            // 経度
 	CreatedAT time.Time `gorm:"autoCreateTime" json:"createdAT"`    // 作成時
 	ImageID   string    `gorm:"varchar(50)" json:"imageID"`         // イメージID
+	Steps     int64     `json:"steps"`                              // 歩数
 }
 
 // テーブル名
 func (Circle) TableName() string {
 	return "Circles"
+}
+
+// 円を取得する関数
+func GetCircle(circleid string) (Circle, error) {
+	var circle Circle
+	return circle, dbconn.Where(&Circle{
+		CircleID: circleid,
+	}).First(&circle).Error
 }
 
 // メンバーに属する円の一覧取得
@@ -43,13 +52,55 @@ func (member *Member) CreateCircle(circle *Circle) error {
 	// 作成時の時間を設定
 	circle.CreatedAT = time.Now()
 
-	return dbconn.Save(circle).Error
+	// 円を作成
+	err := dbconn.Save(circle).Error
+
+	// エラー処理
+	if err != nil {
+		return err
+	}
+
+	// 円のレベルを更新 (関連するチャンクを更新するため)
+	return circle.ChangeLevel(circle.Level)
 }
 
 // 円のレベルを変更する
 func (circle *Circle) ChangeLevel(level int) error {
 	// 変更
 	circle.Level = level
+
+	// ゲームを取得する
+	game, err := GetGame(circle.GameID)
+
+	// エラー処理
+	if err != nil {
+		return err
+	}
+
+	// 円形にチャンクを取得する
+	chunks, err := game.GetCircleChunkByLatLon(circle.Latitude, circle.Longitude,float64(circle.Size))
+
+	// エラー処理
+	if err != nil {
+		return err
+	}
+
+	for _, chunk := range chunks {
+		// チャンクのレベルがこの円のレベル以下なら
+		if chunk.Level <= circle.Level {
+			// チャンクを更新する
+			if err := chunk.ChangeLevel(circle.Level); err != nil {
+				logger.PrintErr(err)
+				return err
+			}
+
+			// オーナーも変更する
+			if err := chunk.ChangeOwner(circle.UserID); err != nil {
+				logger.PrintErr(err)
+				return err
+			}
+		}
+	}
 
 	// 更新
 	return dbconn.Model(circle).Update("level", level).Error
