23667f12b1506b8f4fc32a895218880792765483
mattuu0 <tempacountyt@gmail.com>
2025-10-11 22:19:22 +0900

    fix(backend): メンバー削除APIの不具合を修正

--- containers/game/src/controllers/game_controller.go
+++ containers/game/src/controllers/game_controller.go
@@ -245,8 +245,7 @@ func DeleteTeamHandler(ctx echo.Context) error {
 // メンバーを削除するエンドポイと
 func DeleteMemberHandler(ctx echo.Context) error {
 	// TODO 後ほどミドルウェアからの取得に変更す
-	userId := ctx.Get("UserID").(string)
-	// userId := ctx.Request().Header.Get("UserID")
+	userId := ctx.Request().Header.Get("UserID")
 	gameId := ctx.Request().Header.Get("GameID")
 
 	// サービスに渡す
